b128972f0371eb9efec503e4ef73a5908653a4fe
IvanBrianCruz <ivan4912134@gmail.com>
2025-09-05 18:58:23 -0300

    clase 3

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -23,7 +23,8 @@ const Videos = () => {
     */
   const videoList = [
    "https://www.youtube.com/embed/tJzH0XO8Hjs?si=gVN4WrcKlbUKs5y5",
-   "https://www.youtube.com/embed/E5jYnF1Xv1g?si=Qx6aBbf0A3fxrj46"
+   "https://www.youtube.com/embed/E5jYnF1Xv1g?si=Qx6aBbf0A3fxrj46",
+   "https://www.youtube.com/embed/uuUqN6UsiSI?si=rlYrrCMlTfimjkn5"
   ];
 
   return (
